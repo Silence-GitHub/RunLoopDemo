9d370a7d7ce0487f061c97068a2c84a561dee93e
Kaibo <lukaibolkb@163.com>
2017-03-03 20:40:43 +0800

    First commit

--- RunLoopDemo/ViewController.swift
+++ RunLoopDemo/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCome), userInfo: nil, repeats: true)
+    }
+    
+    func timerCome() {
+        print(#function)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Table view data source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Text"
+        return cell
+    }
+    
+    // MARK: - Scroll view delegate
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        perform(#selector(sourceCome), with: self, afterDelay: 0, inModes: [RunLoopMode.defaultRunLoopMode])
+    }
+    
+    func sourceCome() {
+        print(#function)
+    }
 
 }
 
